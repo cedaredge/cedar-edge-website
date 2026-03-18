0b19e13c3f965e8e114f9b9b29c67dd95a51dd99
cedaredge <cedaredgetechnologies@gmail.com>
2026-03-19 00:46:07 +0530

    Update index.ts

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -3,5 +3,4 @@ export { CourseCatalog } from './course-catalog';
 export { Testimonials } from './testimonials';
 export { LearningPaths } from './learning-paths';
 export { WebMobileDevelopment } from './web-mobile-development';
-export { DigitalMarketing } from './digital-marketing';
 
